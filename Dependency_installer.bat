b6084885ab1218ebf7d81bb2a3405c14316ccfbe
eselznick <ellis@selznick.com>
2023-08-01 12:23:58 -0700

    LC_Demo package

---
+++ Dependency_installer.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8397321d63962e2e5ef029ebe70c43f7229e37a8fd7243a2688a81452d947b18
+size 4101
---
+++ gui.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9577ffaac762234e8386207530948d85d9a201f42a2ae0097fdea1ea29e2587
+size 117
